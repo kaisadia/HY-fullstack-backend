e313d105ea8a0e584563b9d3ef413e988141703a
Kaisa Diakhate <kaisa.diakhate@brightstraining.com>
2023-04-27 05:28:00 -0700

    added frontend build file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let db = require('./db.json')
 const port = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms :body'));
