4ee1d8107ad185b82255aeddaf53aab97d0d8200
Kaisa Diakhate <kaisa.diakhate@brightstraining.com>
2023-05-04 02:26:02 -0700

    added error handling

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-let db = require('./db.json')
-const port = process.env.PORT || 3001
+const port = process.env.PORT 
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -11,50 +12,78 @@ app.use(cors())
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms :body'));
 
-app.get('/api/persons', (req, res) => {
-res.json(db)
-})
+
+const get = app.get('/api/persons', (req, res) => {
+  Person.find({}).then(people => { 
+  res.json(people)}
+)})
 
 app.get('/info', (req, res) => {
-    res.send(`Phonebook has info for ${db.persons.length} people 
+    res.send(`Phonebook has info for 
+    ${get.length} people 
     ${new Date().toString()}`)
     })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    console.log(db.persons)
-    const person = db.persons.find(person => person.id === id)
-    if(!person) {
-        res.status(404).end()
-    } else {
+  Person.findById(req.params.id)
+  .then(person => {
+    if (person) {
     res.json(person)}
-    })
+    else {res.status(404).end()}
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(400).send({ error: 'malformatted id' })
+  })
+
+})
+      
     
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-     db = db.persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons/', (req, res) => {
-   const maxId = db.persons.length > 0 ? Math.max(...db.persons.map(n => n.id)) : 0
-   const person = req.body;
-   person.id = maxId +1
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'name missing' })
+  } else if (body.number === undefined) {
+    return res.status(400).json({ error: 'number missing' })}
 
-   if (!person.name || !person.number) {
-    return res.status(400).json({ 
-      error: 'content missing' 
-    })
-  } else if(db.persons.map(name => name.name).includes(person.name)){
-    return res.status(400).json({ 
-      error: 'name already exists' 
-    })
-  } else { 
-   db.persons =db.persons.concat(person)
-   console.log(person);
-   res.json(person)
-}})
+  const person = new Person({
+    name: body.name,
+    number: body.number 
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+  .catch(error => next(error))
+})
 
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError'){
+  return res.status(400).json({ error: error.message })
+}
+next(error)
+}
+
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
